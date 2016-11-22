464e24d1f967765250cba36fff2f34626fc8af53
broerjuang <juangwiantoro@gmail.com>
2016-11-22 10:00:13 +0700

    server is running

---
+++ server/app.js
@@ -0,0 +1,51 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const fs = require('fs')
+const path = require('path')
+const app = express()
+
+const COMMENT_FILES = path.join(__dirname, 'comments.json')
+
+app.set('port', (process.env.PORT || 3000))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+// cors manual
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Cache-Control', 'no-cache')
+  next()
+})
+
+app.get('/api/comments', (req, res) => {
+  fs.readFile(COMMENT_FILES, (err, data) => {
+    err ? console.log(err) : res.json(JSON.parse(data))
+  })
+})
+
+app.post('/api/comments', (req, res) => {
+  fs.readFile(COMMENT_FILES, (err, data) => {
+    if(err) {
+      console.log(err)
+    }
+    let comments = JSON.parse(data)
+    let newComment = {
+      id: Date.now(),
+      author: req.body.author,
+      text: req.body.text
+    };
+    comments.push(newComment)
+    fs.writeFile(COMMENT_FILES, JSON.stringify(comments, null, 4), (err) => {
+      if(err) {
+        console.log(err)
+      }
+      res.json(comments)
+    })
+  })
+})
+
+app.listen(app.get('port'), () => {
+  console.log('server is running on port 3000')
+})
