35284a3624e005e746a40fe05e0b0caf0ea083ab
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-11-22 09:03:11 +0700

    include all depedencies in app.js

--- server/app.js
+++ server/app.js
@@ -4,6 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const cors = require('cors')
+
+const mongoose = require('mongoose')
+mongoose.connect(procces.env.DATABASE)
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -20,9 +24,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors())
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/api/comments', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
