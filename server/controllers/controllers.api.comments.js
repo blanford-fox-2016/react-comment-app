1e6f51ce7d37a34cf0e3386a2068e54935368c65
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-11-22 09:17:41 +0700

    create controller structure for comment

---
+++ server/controllers/controllers.api.comments.js
@@ -0,0 +1,15 @@
+const Comment = require('../models/models.api.comments')
+
+module.exports = {
+    createComment: () => {
+
+    },
+
+    getAllcomments: (req, res) => {
+
+    },
+
+    deleteComment: (req, res) => {
+
+    }
+}
